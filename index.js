4799fbcf30673eaef45590d69cc6e3a425b40346
Matteo Collina <hello@matteocollina.com>
2022-09-07 23:30:59 +0200

    Move isolates to optionalDependencies
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 'use strict'
 
-const SynchronousWorker = require('@matteo.collina/isolates')
+const importFresh = require('import-fresh')
+let SynchronousWorker
+
+try {
+  SynchronousWorker = require('@matteo.collina/isolates')
+} catch {
+  // do nothing
+}
 
 async function isolate (app, opts) {
-  const worker = new SynchronousWorker({
-    sharedEventLoop: true,
-    sharedMicrotaskQueue: true
-  })
-
   const stopTimeout = opts.stopTimeout || 100
 
   const onError = opts.onError || routeToProcess
   const customizeGlobalThis = opts.customizeGlobalThis || noop
 
-  let _require = worker.createRequire(opts.path)
+  if (SynchronousWorker) {
+    const worker = new SynchronousWorker({
+      sharedEventLoop: true,
+      sharedMicrotaskQueue: true
+    })
 
-  worker.process.on('uncaughtException', onError)
-  worker.globalThis.Error = Error
+    let _require = worker.createRequire(opts.path)
 
-  customizeGlobalThis(worker.globalThis)
+    worker.process.on('uncaughtException', onError)
+    worker.globalThis.Error = Error
 
-  app.register(_require(opts.path), opts)
-  _require = null
+    customizeGlobalThis(worker.globalThis)
 
-  app.addHook('onClose', (_, done) => {
-    // the immediate blocks are needed to ensure that the worker
-    // has actually finished its work before closing
-    setTimeout(() => {
-      worker.stop().then(() => {
-        setImmediate(done)
-      })
-    }, stopTimeout)
-  })
+    app.register(_require(opts.path), opts)
+    _require = null
+
+    app.addHook('onClose', (_, done) => {
+      // the immediate blocks are needed to ensure that the worker
+      // has actually finished its work before closing
+      setTimeout(() => {
+        worker.stop().then(() => {
+          setImmediate(done)
+        })
+      }, stopTimeout)
+    })
+  } else {
+    app.register(importFresh(opts.path), opts)
+  }
 
   function routeToProcess (err) {
     app.log.error({
---
+++ test/no-compiler.test.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const { test } = require('tap')
+const proxyquire = require('proxyquire')
+
+const isolate = proxyquire('../', {
+  '@matteo.collina/isolates': null
+})
+const Fastify = require('fastify')
+const path = require('path')
+
+test('different isolates', async ({ same, teardown }) => {
+  const app = Fastify()
+  teardown(app.close.bind(app))
+
+  app.addHook('onRequest', async function (req) {
+    req.p = Promise.resolve('hello')
+  })
+
+  app.get('/check', async function (req) {
+    return {
+      check: Object.getPrototypeOf(req.p).constructor === Promise
+    }
+  })
+
+  app.register(isolate, {
+    path: path.join(__dirname, '/plugin.js')
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/check'
+    })
+    const data = res.json()
+
+    same(data, { check: true })
+  }
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    const data = res.json()
+
+    // Bummer, we have do not have isolates
+    same(data, { check: true })
+  }
+})
